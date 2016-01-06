98c122d2f0609dbcb9d72f50a1a6cf23bf634170
nerviozzo <raupulus@gmail.com>
2016-01-06 12:47:46 +0100

    Archivo principal con el primer comando php

---
+++ PHP/index.php
@@ -0,0 +1,16 @@
+<HTML>
+    <HEAD>
+        <TITLE>Principal</TITLE>
+				<META charset="UTF-8"/>
+    </HEAD>
+    
+    <BODY>
+        Este es el servidor con PHP activado
+        <BR/>
+        El funcionamiento es correcto si puedes ver lo siguiente:
+				<?php
+					$fecha = date("d/m/Y");
+					echo "La fecha de hoy es $fecha";
+				?>
+    </BODY>
+</HTML>
