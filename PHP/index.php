1ad394231a74593c08a73cacc9c1a178c3ac0673
nerviozzo <raupulus@gmail.com>
2016-01-12 17:50:35 +0100

    Cambio de permisos a 777

--- PHP/index.php
+++ PHP/index.php
@@ -12,5 +12,15 @@
 					$fecha = date("d/m/Y");
 					echo "La fecha de hoy es $fecha";
 				?>
+
+        <P>
+          <UL>
+            <LI><A href="MiFormulario.html">Mi Formulario PHP</A></LI>
+            <LI><A href="ComprobarFormulario.php">Comprobar formularios</A></LI>
+            <LI><A href="./Formularios/formularioSimple.html">Acceder al ejemplo de Formulario Simple de: LIBRO ANAYA</A></LI>
+            <LI><A href="./Formularios/formularioParaValidar.html">Acceder al ejemplo de Formulario Para validar de: LIBRO ANAYA</A></LI>
+            <LI><A href="./Formularios/formularioConImagen.html">Acceder al ejemplo de Formulario con Imagen de: LIBRO ANAYA</A></LI>
+          </UL>
+        </P>
     </BODY>
 </HTML>
