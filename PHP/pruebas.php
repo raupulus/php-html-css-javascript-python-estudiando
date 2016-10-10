9962aba31a6f2e9112e3a5d9b27c96205ac3aca6
Fryntiz <tecnico@fryntiz.es>
2016-10-10 12:35:34 +0200

    plantilla basica para pruebas

---
+++ PHP/pruebas.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<?php
+
+?>
+<HTML lang="es">
+<HEAD>
+	<TITLE>prueba</TITLE>
+	<META charset="utf-8"/>
+	<META name="description" content="prueba"/>
+	<META name="keywords" content="ejemplo, plantilla, programación, Raúl Caro Pastorino, Fryntiz"/>
+	<META name="author" content="Raúl Caro Pastorino"/>
+	<LINK rel="shortcut icon" href="./images/favicon.png"/>
+	<LINK rel="stylesheet" href="./CSS/styles.css"/>
+	<LINK rel="stylesheet" href="./CSS/1000.css"/>
+	<LINK rel="stylesheet" href="./CSS/+1000.css"/>
+	<SCRIPT src="JS/scripts.js"></SCRIPT>
+</HEAD>
+
+<BODY>
+	<H1>prueba</H1>
+	<P>prueba</P>
+	<DIV id="CajaGeneral">
+		<?php
+
+		?>
+	</DIV>
+<?php
+
+?>
+</BODY>
+</HTML>
